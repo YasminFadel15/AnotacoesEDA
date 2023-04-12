35a29a20dc76c9fe88a495765dc05ef729faf5b1
Yasmin Fadel <110503796+YasminFadel15@users.noreply.github.com>
2023-04-12 14:14:55 -0300

    Update deques.c

--- deques.c
+++ deques.c
@@ -1,100 +1,142 @@
 //Deques: filas encadeadas
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-typedef struct{
-    struct No* anterior;
-    struct No* proximo;
+typedef struct {
     int valor;
-} No;
+    struct Elemento* proximo;
+    struct Elemento* anterior;
+} Elemento;
 
-typedef struct{
-    No* cabeca;
-    No* cauda;
+typedef struct {
+    Elemento* cabeca;
+    Elemento* cauda;
 } Deque;
 
-
-Deque* criar(){
+Deque* criar() {
     Deque* deque = malloc(sizeof(Deque));
-    deque -> cabeca = NULL;
-    deque -> cauda = NULL:
+    deque->cabeca = NULL;
+    deque->cauda = NULL;
 
     return deque;
 }
 
-int vazia(Deque* deque){
-    return deque -> cabeca == NULL && deque -> cauda == NULL;
+int vazia(Deque* deque) {
+    return deque->cabeca == NULL && deque->cauda == NULL;
 }
 
+void adicionarInicio(Deque* deque, int valor) {
+    Elemento* elemento = malloc(sizeof(Elemento));
+    elemento->valor = valor;
+    elemento->anterior = NULL;
+    elemento->proximo = (struct Elemento*) deque->cabeca;
 
-void adicionarFim(Deque* deque, int valor){
-    No* no = malloc(sizeof(No));
-    no -> valor = valor;
-    no -> proximo = NULL;
-    no -> anterior = deque -> cauda;
-    
-    if(!vazia(deque))
-        deque -> cauda -> proximo = no;
-    else
-        deque -> cabeca = no;
+    if (vazia(deque)) {
+        deque->cauda = elemento;
+    }
 
-    deque -> cauda = no;
+    deque->cabeca = elemento;
 }
 
+void adicionarFim(Deque* deque, int valor) {
+    Elemento* elemento = malloc(sizeof(Elemento));
+    elemento->valor = valor;
+    elemento->proximo = NULL;
+    elemento->anterior = (struct Elemento*) deque->cauda;
 
-void adicionarInicio(Deque* deque, int valor){
-    No* no = malloc(sizeof(No));
-    no -> valor = valor;
-    no -> proximo = NULL;
-    no -> anterior = deque -> cabeca;
-    
-    if(!vazia(deque))
-        deque -> cabeca -> proximo = no;
-    else
-        deque -> cauda = no;
+    if (vazia(deque)) {
+        deque->cabeca = elemento;
+    } else {        
+        deque->cauda->proximo = (struct Elemento*) elemento;
+    }
 
-    deque -> cauda = no;
+    deque->cauda = elemento;
 }
-
 
 int removerFim(Deque* deque) {
     if (vazia(deque)) {
-        printf("Deque vazio\n");
+        printf("Deque vazia\n");
+        return 0;
+    }
+    
+    int valor = deque->cauda->valor;
+    Elemento* elemento = (Elemento*) deque->cauda;
+
+    deque->cauda = (Elemento*) elemento->anterior;
+    free(elemento);
+
+    if (deque->cauda == NULL) {
+        deque->cabeca = NULL;
+    } else {
+        deque->cauda->proximo = NULL;
+    }
+
+    return valor;
+
+}
+
+int removerInicio(Deque* deque) {
+    if (vazia(deque)) {
+        printf("Deque vazia\n");
         return 0;
     }
 
-    No* no = deque -> cauda;
-    int valor = no -> valor;
-    deque -> cauda = (No*) deque -> cauda -> anterior;
+    int valor = deque->cabeca->valor;
+    Elemento* elemento = (Elemento*) deque->cabeca;
 
-    if(deque -> cauda == NULL)
-        deque -> cabeca = NULL;
-    else
-        deque -> cauda -> anterior = NULL;
-        
-    free(no);
+    deque->cabeca = (Elemento*) elemento->proximo;
+    free(elemento);
+
+    if (deque->cabeca == NULL) {
+        deque->cauda = NULL;
+    } else {
+        deque->cabeca->anterior = NULL;
+    }
 
     return valor;
 }
 
+void limpar(Deque* deque) {
+    while (!vazia(deque)) {
+        removerInicio(deque);
+    }
+}
 
-int removerInicio(Deque* deque) {
-    if (vazia(deque)) {
-        printf("Deque vazio\n");
-        return 0;
+void percorrer(Deque* deque) {
+    Elemento* elemento = deque->cabeca;
+
+    while (elemento != NULL) {
+        printf("%d ", elemento->valor);
+        elemento = (Elemento*) elemento->proximo;
     }
 
-    No* no = deque -> cabeca;
-    int valor = no -> valor;
-    deque -> cabeca = (No*) deque -> cabeca -> proximo;
+    printf("\n");
+}
 
-    if(deque -> cabeca == NULL)
-        deque -> cauda = NULL;
-    else
-        deque -> cabeca -> anterior = NULL;
+int main() {
+    Deque* deque = criar();
 
-    free(no);
+    for (int i = 0; i < 5; i++) {
+        if (i % 2 == 0) {
+            adicionarFim(deque, i);
+        } else {
+            adicionarInicio(deque, i);
+        }
+    }
 
-    return valor;
+    percorrer(deque);
+    printf("\n");
+    
+    removerInicio(deque);
+    removerFim(deque);
+
+    percorrer(deque);
+    printf("\n");
+    
+    adicionarInicio(deque, 60);
+    adicionarFim(deque, 70);
+
+    percorrer(deque);
+    printf("\n");
 }
